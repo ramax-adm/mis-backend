243b396c2fe858b41c4618d4ff9cbc9be8c82f4d
Lucas Ribeiro <lucassantana00@gmail.com>
2025-08-05 16:12:43 -0300

    feat/wip: analytical method for invoices

--- src/modules/sales/controllers/sales-invoices.controller.ts
+++ src/modules/sales/controllers/sales-invoices.controller.ts
@@ -1,12 +1,142 @@
-import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { InvoicesNfTypesEnum } from '../enums/invoices-nf-types.enum';
 import { InvoicesSituationsEnum } from '../enums/invoices-situations.enum';
 import { SalesInvoicesService } from '../services/sales-invoices.service';
+import { DataSource } from 'typeorm';
+import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/modules/auth/guards/user-roles.guard';
 
 @Controller('sales/invoice')
 export class SalesInvoicesController {
-  constructor(private readonly salesInvoicesService: SalesInvoicesService) {}
+  constructor(
+    private readonly dataSource: DataSource,
 
+    private readonly salesInvoicesService: SalesInvoicesService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('/filters/cfops')
+  @HttpCode(HttpStatus.OK)
+  async getCfops(
+    @Query('companyCode') companyCode: string,
+    @Query('startDate') startDate: Date,
+    @Query('endDate') endDate: Date,
+  ) {
+    const qb = this.dataSource
+      .createQueryBuilder()
+      .select([
+        'si.cfop_code',
+        'UPPER(si.cfop_description) AS cfop_description',
+      ])
+      .from('sensatta_invoices', 'si')
+      .distinct(true)
+      .where('1=1')
+      .orderBy('si.cfop_code', 'ASC');
+
+    if (companyCode) {
+      qb.andWhere('si.company_code = :companyCode', { companyCode });
+    }
+    if (startDate) {
+      qb.andWhere('si.date >= :startDate', { startDate });
+    }
+    if (endDate) {
+      qb.andWhere('si.date <= :endDate', { endDate });
+    }
+
+    const results = await qb.getRawMany<{
+      cfop_code: string;
+      cfop_description: string;
+    }>();
+
+    return results.map((i) => ({
+      key: i.cfop_code,
+      label: `${i.cfop_code} - ${i.cfop_description}`,
+      value: i.cfop_code,
+    }));
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('/filters/clients')
+  @HttpCode(HttpStatus.OK)
+  async getClients(
+    @Query('companyCode') companyCode: string,
+    @Query('startDate') startDate: Date,
+    @Query('endDate') endDate: Date,
+  ) {
+    const qb = this.dataSource
+      .createQueryBuilder()
+      .select(['si.client_code', 'si.client_name'])
+      .from('sensatta_invoices', 'si')
+      .distinct(true)
+      .where('1=1')
+      .orderBy('si.client_name', 'ASC');
+
+    if (companyCode) {
+      qb.andWhere('si.company_code = :companyCode', { companyCode });
+    }
+    if (startDate) {
+      qb.andWhere('si.date >= :startDate', { startDate });
+    }
+    if (endDate) {
+      qb.andWhere('si.date <= :endDate', { endDate });
+    }
+
+    const results = await qb.getRawMany<{
+      client_code: string;
+      client_name: string;
+    }>();
+
+    return results.map((i) => ({
+      key: i.client_code,
+      label: i.client_name,
+      value: i.client_code,
+    }));
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('/filters/nf-situations')
+  @HttpCode(HttpStatus.OK)
+  async getNfSituations(
+    @Query('companyCode') companyCode: string,
+    @Query('startDate') startDate: Date,
+    @Query('endDate') endDate: Date,
+  ) {
+    const qb = this.dataSource
+      .createQueryBuilder()
+      .select(['si.nf_situation'])
+      .from('sensatta_invoices', 'si')
+      .distinct(true)
+      .where('1=1');
+
+    if (companyCode) {
+      qb.andWhere('si.company_code = :companyCode', { companyCode });
+    }
+    if (startDate) {
+      qb.andWhere('si.date >= :startDate', { startDate });
+    }
+    if (endDate) {
+      qb.andWhere('si.date <= :endDate', { endDate });
+    }
+
+    const results = await qb.getRawMany<{
+      nf_situation: string;
+    }>();
+
+    return results.map((i) => ({
+      key: i.nf_situation,
+      label: i.nf_situation,
+      value: i.nf_situation,
+    }));
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('resumed')
   @HttpCode(HttpStatus.OK)
   async getResumedData(
@@ -15,6 +145,7 @@ export class SalesInvoicesController {
     @Query('endDate') endDate: Date,
   ) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('analytical')
   @HttpCode(HttpStatus.OK)
   async getAnalyticalData(
--- src/modules/sales/services/sales-invoices.service.ts
+++ src/modules/sales/services/sales-invoices.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { InvoicesNfTypesEnum } from '../enums/invoices-nf-types.enum';
 import { InvoicesSituationsEnum } from '../enums/invoices-situations.enum';
+import { GetInvoicesItem } from '../types/get-invoices.type';
+import { NumberUtils } from '@/modules/utils/services/number.utils';
 
 @Injectable()
 export class SalesInvoicesService {
@@ -18,7 +20,7 @@ export class SalesInvoicesService {
         and si.nf_number ilike '%' || :nfNumber || '%'
         and si.nf_situation = :nfSituation
    */
-  getAnalyticalData({
+  async getAnalyticalData({
     companyCode,
     startDate,
     endDate,
@@ -37,7 +39,7 @@ export class SalesInvoicesService {
     nfNumber: string;
     nfSituation: InvoicesSituationsEnum;
   }) {
-    return this.getInvoices({
+    const data = await this.getInvoices({
       companyCode,
       startDate,
       endDate,
@@ -47,9 +49,34 @@ export class SalesInvoicesService {
       nfNumber,
       nfSituation,
     });
+
+    // nf quantity
+    const nfSet = new Set();
+    data.forEach((i) => nfSet.add(i.nfNumber));
+
+    const totals = data.reduce(
+      (acc, item) => ({
+        quantity: acc.quantity,
+        productQuantity: acc.productQuantity + 1,
+        boxAmount: NumberUtils.nb2(acc.boxAmount + item.boxAmount),
+        weightInKg: NumberUtils.nb2(acc.weightInKg + item.weightInKg),
+        unitPrice: NumberUtils.nb2(acc.unitPrice + item.unitPrice),
+        totalPrice: NumberUtils.nb2(acc.totalPrice + item.totalPrice),
+      }),
+      {
+        quantity: nfSet.size,
+        productQuantity: 0,
+        boxAmount: 0,
+        weightInKg: 0,
+        unitPrice: 0,
+        totalPrice: 0,
+      },
+    );
+
+    return { totals, data };
   }
 
-  private getInvoices({
+  private async getInvoices({
     companyCode,
     startDate,
     endDate,
@@ -67,7 +94,7 @@ export class SalesInvoicesService {
     nfType?: InvoicesNfTypesEnum;
     nfNumber?: string;
     nfSituation?: InvoicesSituationsEnum;
-  }) {
+  }): Promise<GetInvoicesItem[]> {
     const qb = this.datasource
       .createQueryBuilder()
       .select(['si.*', 'sc.name AS company_name'])
@@ -110,6 +137,52 @@ export class SalesInvoicesService {
       qb.andWhere('si.date <= :endDate', { endDate });
     }
 
-    return qb.getRawMany();
+    const results = await qb.getRawMany<{
+      id: string;
+      date?: Date;
+      nf_type?: string;
+      client_type_code?: string | null;
+      client_type_name?: string | null;
+      company_code?: string;
+      company_name: string;
+      cfop_code?: string;
+      cfop_description?: string;
+      nf_number?: string;
+      request_id?: string;
+      client_code?: string;
+      client_name?: string;
+      product_code?: string;
+      product_name?: string;
+      box_amount?: number;
+      weight_in_kg?: number;
+      unit_price?: number;
+      total_price?: number;
+      created_at?: Date; // ou `Date` se for convertido
+      nf_situation: string;
+    }>();
+
+    return results.map((i) => ({
+      id: i.id,
+      date: i.date,
+      nfSituation: i.nf_situation,
+      nfType: i.nf_type,
+      clientTypeCode: i.client_type_code,
+      clientTypeName: i.client_type_name,
+      companyCode: i.company_code,
+      companyName: i.company_name,
+      cfopCode: i.cfop_code,
+      cfopDescription: i.cfop_description,
+      nfNumber: i.nf_number,
+      requestId: i.request_id,
+      clientCode: i.client_code,
+      clientName: i.client_name,
+      productCode: i.product_code,
+      productName: i.product_name,
+      boxAmount: i.box_amount,
+      weightInKg: i.weight_in_kg,
+      unitPrice: i.unit_price,
+      totalPrice: i.total_price,
+      createdAt: i.created_at,
+    }));
   }
 }
