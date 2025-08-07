bc33941e423b8cfd7b4c35cececdea75ab29a67d
Lucas Ribeiro <lucassantana00@gmail.com>
2025-08-07 12:49:47 -0300

    feat: disacouple and and make available constants on business audit

--- src/modules/business-audit/controllers/business-audit.controller.ts
+++ src/modules/business-audit/controllers/business-audit.controller.ts
@@ -9,10 +9,26 @@ import {
 import { BusinessAuditService } from '../services/business-audit.service';
 import { JwtAuthGuard } from '@/modules/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '@/modules/auth/guards/user-roles.guard';
+import { CONSIDERED_CFOPS } from '../constants/considered-cfops';
+import { CONSIDERED_NF_SITUATIONS } from '../constants/considered-nf-situations';
 
 @Controller('business-audit')
 export class BusinessAuditController {
   constructor(private readonly businessAuditService: BusinessAuditService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('constants/considered-cfops')
+  @HttpCode(HttpStatus.OK)
+  getConsideredCfops() {
+    return CONSIDERED_CFOPS;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('constants/nf-situations')
+  @HttpCode(HttpStatus.OK)
+  getConsideredNfSituations() {
+    return CONSIDERED_NF_SITUATIONS;
+  }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('resumed')
