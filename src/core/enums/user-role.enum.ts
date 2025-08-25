0a84f6f4dab81df7bebfbc691fdfce6f6a5fbffd
Lucas Ribeiro <lucassantana00@gmail.com>
2025-08-25 18:57:49 -0300

    fix:[32]:users crud

--- src/core/enums/user-role.enum.ts
+++ src/core/enums/user-role.enum.ts
@@ -1,6 +1,14 @@
+// TODO: change to UserDepartment
 export enum UserRole {
   Admin = 'admin',
   Directory = 'directory',
+  Financial = 'financial',
   Commercial = 'commercial',
   Industry = 'industry',
+  Accounting = 'accounting',
+  Marketing = 'marketing',
+  Administrative = 'administrative',
+  Tecnology = 'tecnology',
+  Risk = 'risk',
+  Other = 'other',
 }
--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -72,8 +72,8 @@ export class UserController {
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
-  findAll(@Query('roles') roles?: string[]) {
-    return this.userService.findAll(roles);
+  findAll(@Query('username') username?: string) {
+    return this.userService.findAll(username);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
@@ -96,5 +96,67 @@ export class UserController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('/filters/departments')
+  getDepartments() {
+    return [
+      {
+        label: 'Admin',
+        value: UserRole.Admin,
+        key: UserRole.Admin,
+      },
+      {
+        label: 'Diretoria',
+        value: UserRole.Directory,
+        key: UserRole.Directory,
+      },
+      {
+        label: 'Financeiro',
+        value: UserRole.Financial,
+        key: UserRole.Financial,
+      },
+      {
+        label: 'Comercial',
+        value: UserRole.Commercial,
+        key: UserRole.Commercial,
+      },
+      {
+        label: 'Industria',
+        value: UserRole.Industry,
+        key: UserRole.Industry,
+      },
+      {
+        label: 'Contabilidade',
+        value: UserRole.Accounting,
+        key: UserRole.Accounting,
+      },
+      {
+        label: 'Marketing',
+        value: UserRole.Marketing,
+        key: UserRole.Marketing,
+      },
+      {
+        label: 'Administrativo',
+        value: UserRole.Administrative,
+        key: UserRole.Administrative,
+      },
+      {
+        label: 'TI',
+        value: UserRole.Tecnology,
+        key: UserRole.Tecnology,
+      },
+      {
+        label: 'Risco',
+        value: UserRole.Risk,
+        key: UserRole.Risk,
+      },
+      {
+        label: 'Outros',
+        value: UserRole.Other,
+        key: UserRole.Other,
+      },
+    ];
   }
 }
